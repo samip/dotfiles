dd544b6bf5731fe4fe2975c455d56ae0aae961fd
Sami Pellinen <sami.m.pellinen@gmail.com>
2022-06-04 14:57:54 +0300

    zsh config

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
-apt-get install -y tmux stew tig neovim xclip bat git fzf
+apt-get install -y tmux stew tig neovim xclip bat git fzf zsh
 
 # https://github.com/mwh/dragon
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git config --global alias.st status
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.ll log --oneline
 git config --global alias.d diff
 
-if ! command -v lsd &> /dev/null
+if ! command -v lsd > /dev/null
 then
     lsd_deb_name="lsd_0.21.0_amd64.deb"
     lsd_repo_url="https://github.com/Peltoche/lsd"
@@ -23,5 +24,4 @@ fi
 cat "$PWD/init/.bash_history" >> ~/.bash_history
 cat "$PWD/init/.config/shortcut-locations" >> ~/.config/shortcut-locations
 
-# source ~/.bashrc
 tmux source-file ~/.tmux.conf
